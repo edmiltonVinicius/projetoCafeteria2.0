94605bf558498be69974b2cde7df98cbcfc9bc3f
Edmilton Vinicius <Edmilton Vinicius>
2019-09-20 20:19:45 -0300

    slide pronto, tabela incompleta

---
+++ js/slide.js
@@ -0,0 +1,46 @@
+let arrayImagens = new Array("imagens/carrosel01.jpg", "imagens/carrosel02.jpg", "imagens/carrosel03.jpg")
+let verficiador = 0
+let imgHtml = document.getElementById('imagens')
+
+function avancarSlider() {
+    if (this.verficiador == 0) {
+        imgHtml.src = arrayImagens[0]
+        this.verficiador = 1
+    } else if (this.verficiador == 1) {
+        imgHtml.src = arrayImagens[1]
+        this.verficiador = 2
+    }
+    else {
+        imgHtml.src = arrayImagens[2]
+        this.verficiador = 0
+    }
+}
+function voltarSlider() {
+    if (this.verficiador == 0) {
+        imgHtml.src = arrayImagens[2]        
+        this.verficiador = 2
+    } else if (this.verficiador == 1) {
+        imgHtml.src = arrayImagens[1]       
+         this.verficiador = 0
+    }
+    else {
+        imgHtml.src = arrayImagens[0]        
+        this.verficiador = 1
+    }
+}
+
+function sliderAutomatico1() {
+    document.getElementById('imagens').src = arrayImagens[0]
+    this.verficiador = 0
+    setTimeout("sliderAutomatico2()", 2000)
+}
+function sliderAutomatico2() {
+    document.getElementById('imagens').src = arrayImagens[1]
+    this.verficiador = 1
+    setTimeout("sliderAutomatico3()", 2000)
+}
+function sliderAutomatico3() {
+    document.getElementById('imagens').src = arrayImagens[2]
+    this.verficiador = 2
+    setTimeout("sliderAutomatico1()", 2000)
+}
