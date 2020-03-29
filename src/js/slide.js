8334e0b1692bb0a4b6d2421f2cec390759605cb6
Edmilton Vinicius <edmilton.vinicius2@gmail.com>
2020-03-29 02:21:01 -0300

    correcao bug no slide

--- src/js/slide.js
+++ src/js/slide.js
@@ -1,46 +1,46 @@
 let arrayImagens = ["static/dest-img/carrosel01.jpg", "static/dest-img/carrosel02.jpg", "static/dest-img/carrosel03.jpg"]
-let verficiador = 0
+window.verficiador = 0
 let imgHtml = document.getElementById('imagens')
 
 function avancarSlider() {
-    if (this.verficiador == 0) {
+    if (window.verficiador == 0) {
         imgHtml.src = arrayImagens[0]
-        this.verficiador = 1
-    } else if (this.verficiador == 1) {
+        window.verficiador = 1
+    } else if (window.verficiador == 1) {
         imgHtml.src = arrayImagens[1]
-        this.verficiador = 2
+        window.verficiador = 2
     }
     else {
         imgHtml.src = arrayImagens[2]
-        this.verficiador = 0
+        window.verficiador = 0
     }
 }
 function voltarSlider() {
-    if (this.verficiador == 0) {
+    if (window.verficiador == 0) {
         imgHtml.src = arrayImagens[2]        
-        this.verficiador = 2
-    } else if (this.verficiador == 1) {
+        window.verficiador = 2
+    } else if (window.verficiador == 1) {
         imgHtml.src = arrayImagens[1]       
-         this.verficiador = 0
+         window.verficiador = 0
     }
     else {
         imgHtml.src = arrayImagens[0]        
-        this.verficiador = 1
+        window.verficiador = 1
     }
 }
 
 function sliderAutomatico1() {
     document.getElementById('imagens').src = arrayImagens[0]
-    this.verficiador = 0
+    window.verficiador = 0
     setTimeout("sliderAutomatico2()", 2000)
 }
 function sliderAutomatico2() {
     document.getElementById('imagens').src = arrayImagens[1]
-    this.verficiador = 1
+    window.verficiador = 1
     setTimeout("sliderAutomatico3()", 2000)
 }
 function sliderAutomatico3() {
     document.getElementById('imagens').src = arrayImagens[2]
-    this.verficiador = 2
+    window.verficiador = 2
     setTimeout("sliderAutomatico1()", 2000)
 }
